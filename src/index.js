2b0ca238b2013dcbba3ec50eb346ddfc95e82157
Tyla Jeffs <tylajeffs@gmail.com>
2023-03-13 16:03:30 -1000

    fix bugs, delete unnecessary files

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import Highcharts from "highcharts";
 import HighchartsSankey from "highcharts/modules/sankey";
 import HighchartsReact from "highcharts-react-official";
 
-import data from "./data/asylumSankey";
+import data from "./data/totalSankey";
 import "./styles.css";
 
 HighchartsSankey(Highcharts);
