8b72fb3bfdcf4cfecb2a1de7778e568018af6c6b
Tyla Jeffs <tylajeffs@gmail.com>
2023-03-23 23:08:20 -1000

    add chart data for Asia Pacific to East and Horn of Africa

--- src/pages/ap-to-e-horna.jsx
+++ src/pages/ap-to-e-horna.jsx
@@ -33,20 +33,14 @@ const App = () => {
         {
             keys: ["from", "to", "weight"],
             data: [
-                ["Afghanistan","Nigeria",5  ],
-                ["Cambodia","Cote d'Ivoire",8  ],
-                ["Iran (Islamic Rep. of)","Chad",5  ],
-                ["Iran (Islamic Rep. of)","Cameroon",5  ],
-                ["Iran (Islamic Rep. of)","Cote d'Ivoire",7  ],
-                ["Iran (Islamic Rep. of)","Nigeria",5  ],
-                ["Sri Lanka","Benin",5  ],
-                ["Sri Lanka","Guinea",10  ],
-                ["Pakistan","Benin",5  ],
-                ["Pakistan","Ghana",60  ],
-                ["Pakistan","Liberia",5  ],
-                ["Pakistan","Mali",11  ],
-                ["Viet Nam","Cote d'Ivoire",5  ],
-                ["Uzbekistan","Ghana",9  ]
+                ["Afghanistan","Kenya",12  ],
+                ["Bangladesh","Somalia",6  ],
+                ["Iran (Islamic Rep. of)","United Rep. of Tanzania",5  ],
+                ["Iran (Islamic Rep. of)","Uganda",5  ],
+                ["Sri Lanka","Kenya",9  ],
+                ["Pakistan","Burundi",9  ],
+                ["Pakistan","Kenya",9  ],
+                ["Pakistan","Uganda",227  ]
               ],
             name: "Asia Pacific to East and Horn of Africa"
           }
