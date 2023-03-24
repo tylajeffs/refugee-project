c03ed2ad478bb7352f558a5059deb7af06e54ed7
Tyla Jeffs <tylajeffs@gmail.com>
2023-03-23 21:18:10 -1000

    add the charts to the correct routing

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./pages/Layout";
 import Home from "./pages/Home";
 import DependencyWheelPage from "./pages/dependencyWheel/dependencyWheel";
-import SankeyDiagramPage from "./pages/sankeyDiagram/sankeyDiagram";
+import SankeyDiagramPage from "./pages/sankeyDiagram";
 
 
 export default function App() {
--- src/pages/sankeyDiagram/sankeyDiagram.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-function SankeyPage() {
-    return (
-        <h1>this is the Sankey Page</h1>
-    );
-}
-
-export default SankeyPage;
